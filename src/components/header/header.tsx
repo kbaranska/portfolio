2f8302afecf0542cf12494a9f5961264eb15e66d
Klaudia Barańska <klaudiabaranska11@gmail.com>
2020-10-12 21:05:47 +0200

    Config project

---
+++ src/components/header/header.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export function Header() {
+    return (
+        <div id="header">
+            <h1> Klaudia Barańska - Portfolio </h1>
+        </div>)
+}
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+rules: [
+  // Whatever other rules you have here...
+  {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      'style-loader',
+      // Translates CSS into CommonJS
+      'css-loader',
+      // Compiles Sass to CSS
+      'sass-loader',
+    ],
+  },
+]
